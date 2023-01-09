e3d0f0e3141972649160973ccb5e9c9412f3017d
Leo <leosilvatanabi@gmail.com>
2023-01-09 09:42:49 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -14,6 +14,6 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Departament> Departament { get; set; }
+        public DbSet<Departament> Departament { get; set; }
     }
 }
