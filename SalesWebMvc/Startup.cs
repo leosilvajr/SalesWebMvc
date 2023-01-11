68ab28e98a8e3405e57dd3f9e7d2df71e349126f
Leo <leosilvatanabi@gmail.com>
2023-01-11 12:02:12 -0300

    Implementing Simple Search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -47,6 +47,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>(); //Nosso serviço agora pode ser injetado em outras classes.
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
 
         }                                            //Nome do Projeto Assembly
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
